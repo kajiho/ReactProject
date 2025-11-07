25d0d1fb3108f445c297bca57ff8b58dfd62d515
kajiho <hikmatchy10@gmail.com>
2025-11-07 08:42:03 +0545

    first commit

---
+++ reactproject/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+  <dev>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  </dev>
+  );
+};
+
+export default App;
